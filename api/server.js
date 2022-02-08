ec94b1ad65e0675b613e09631f8a80ccb2726754
Markeisha <markeisha94@gmail.com>
2022-02-08 11:28:58 -0600

    completed users model

--- api/server.js
+++ api/server.js
@@ -1,2 +1,23 @@
 // implement your server here
 // require your posts router and connect it here
+// BREAK UP THIS MONOLITHIC FILE USING ROUTES
+// BREAK UP THIS MONOLITHIC FILE USING ROUTES
+// BREAK UP THIS MONOLITHIC FILE USING ROUTES
+const express = require('express');
+
+const server = express();
+
+const usersRouter = require('./posts/posts-router');
+
+server.use(express.json());
+
+server.use('/api/posts', usersRouter);
+
+
+server.use('*', (req, res) => {
+  res.status(404).send(`
+    <p>Cant find that resource</p>
+  `);
+});
+
+module.exports = server;
